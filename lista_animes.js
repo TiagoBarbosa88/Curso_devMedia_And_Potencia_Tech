bb3018ec7d451887322a1bb9711db41d37d89239
TiagoBarbosa88 <sombr43@gmail.com>
2023-11-26 16:56:47 -0300

    Projeto de Funções de Array

---
+++ lista_animes.js
@@ -0,0 +1,11 @@
+const imprime_itens = (animes) => {
+
+  for( const anime of animes ){
+      console.log(anime);
+  }
+
+}
+
+let animes = ['Samurai X', 'Naruto', 'Death Note', 'Tokyo Ghoul', 'Bleach'];
+
+imprime_itens(animes);
